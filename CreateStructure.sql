76410a18f54b459df8d85110d425f678c215ad38
Ivan-Dell <vanbog@mail.ru>
2023-07-23 15:21:42 +0300

    updated CreateStructure.sql

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -1,54 +1,49 @@
-use [ics-it_tests];
+USE [ics-it_tests];
 
-if OBJECT_ID('dbo.Basket', 'U') is not null
-drop table dbo.Basket;
+IF OBJECT_ID('dbo.Basket', 'U') IS NOT NULL
+DROP TABLE dbo.Basket;
 
-if OBJECT_ID('dbo.SKU', 'U') is not null
-drop table dbo.SKU;
+IF OBJECT_ID('dbo.SKU', 'U') IS NOT NULL
+DROP TABLE dbo.SKU;
 
-if OBJECT_ID('dbo.Family', 'U') is not null
-drop table dbo.Family;
+IF OBJECT_ID('dbo.Family', 'U') IS NOT NULL
+DROP TABLE dbo.Family;
 
-create table dbo.SKU
+CREATE TABLE dbo.SKU
 (
-ID_identity int,
-Code AS 's' + ID_identity
-	constraint AK_Code unique(Code),
-Name varchar not null,
-constraint PK_SKU
-	primary key(ID_identity)
+ ID_identity INT IDENTITY(1,1),
+ Code AS 's' + CAST(ID_identity AS VARCHAR),
+ Name VARCHAR(max) NOT NULL,
+ CONSTRAINT AK_Code UNIQUE(Code),
+ CONSTRAINT PK_SKU PRIMARY KEY(ID_identity)
 );
 
-create table dbo.Family
+CREATE TABLE dbo.Family
 (
-ID_identity int,
-SurName varchar not null,
-BudgetValue money,
-constraint PK_Family
-	primary key(ID_identity)
+ ID_identity INT IDENTITY(1,1),
+ SurName VARCHAR(max) NOT NULL,
+ BudgetValue MONEY,
+ CONSTRAINT PK_Family PRIMARY KEY(ID_identity)
 );
 
-create table dbo.Basket
+CREATE TABLE dbo.Basket
 (
-ID_identity int,
-ID_SKU int 
-	constraint FK_Basket_SKU
-	foreign key(ID_identity)
-	references dbo.SKU(ID_identity),
-ID_Family int 
-	constraint FK_Basket_Family
-	foreign key(ID_identity)
-	references dbo.Family(ID_identity),
-Quantity int 
-	constraint CHK_Basket_Quantity
-	check (Quantity > 0),
-Value int 
-	constraint CHK_Basket_Value
-	check (Value > 0),
-PurchaseDate datetime 
-	constraint DFT_Basket_PurchaseDate
-	default(SYSDATETIME()) for PurchaseDate,
-DiscountValue numeric,
-constraint PK_Basket
-	primary key(ID_identity)
+ ID_identity INT IDENTITY(1,1),
+ ID_SKU INT,	
+ ID_Family INT,
+ Quantity INT 
+	CONSTRAINT CHK_Basket_Quantity
+	CHECK (Quantity > 0),
+ Value INT 
+	CONSTRAINT CHK_Basket_Value
+	CHECK (Value > 0),
+ PurchaseDate DATETIME DEFAULT(SYSDATETIME()),
+ DiscountValue NUMERIC(18,5),
+ CONSTRAINT FK_Basket_SKU 
+	FOREIGN KEY(ID_SKU)
+	REFERENCES dbo.SKU(ID_identity),
+ CONSTRAINT FK_Basket_Family
+	FOREIGN KEY(ID_Family)
+	REFERENCES dbo.Family(ID_identity),
+ CONSTRAINT PK_Basket PRIMARY KEY(ID_identity)
 );
