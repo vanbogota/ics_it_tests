dccb4bdeaf04eaad8d5bea35a1988aac9ae3acb8
Ivan-Dell <vanbog@mail.ru>
2023-07-24 17:33:25 +0300

    code decoration updated in ViewSKUPrice

--- dbo.vw_SKUPrice.sql
+++ dbo.vw_SKUPrice.sql
@@ -1,11 +1,15 @@
 use [ics-it_tests]
 
-IF OBJECT_ID('dbo.vw_SKUPrice') IS NOT NULL
-DROP VIEW dbo.vw_SKUPrice;
-GO
+if OBJECT_ID('dbo.vw_SKUPrice') is not null
+	drop view dbo.vw_SKUPrice;
+go
 
-CREATE VIEW dbo.vw_SKUPrice
-AS
-SELECT ID_identity, Code, Name, dbo.udf_GetSKUPrice(ID_identity) AS Price
-FROM dbo.SKU
+create view dbo.vw_SKUPrice
+as
+select 
+	ID_identity, 
+	Code, 
+	Name, 
+	dbo.udf_GetSKUPrice(ID_identity) as Price
+from dbo.SKU
 
