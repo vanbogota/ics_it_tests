c6b49f47c81c1b65a7dfb307ec58122e248cc4ca
Ivan-Dell <vanbog@mail.ru>
2023-08-01 00:31:07 +0300

    codestyle updated

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -1,18 +1,21 @@
 use [ics-it_tests]
 
-if OBJECT_ID('dbo.udf_GetSKUPrice') is not null 
-	drop function dbo.udf_GetSKUPrice;
+if object_id('dbo.udf_GetSKUPrice') is not null 
+	drop function dbo.udf_GetSKUPrice
+
 go
 
-create function dbo.udf_GetSKUPrice(@ID_SKU int)
+create function dbo.udf_GetSKUPrice (@ID_SKU int)
 returns decimal(18, 2)
 as
 begin
-	declare 
-		@price decimal(18, 2);
-	select 
-		@price = cast(sum(b.Value) as decimal(18, 2))/cast(sum(b.Quantity) as decimal(18,2))
-	from dbo.Basket b
-	where b.ID_SKU = @ID_SKU;
-	return @price;
-end;
+declare 
+	@Price decimal(18, 2)
+select 
+	@Price = cast(sum(b.Value) as decimal(18, 2))/cast(sum(b.Quantity) as decimal(18,2))
+from dbo.Basket as b
+where b.ID_SKU = @ID_SKU
+
+return 
+	@Price
+end
