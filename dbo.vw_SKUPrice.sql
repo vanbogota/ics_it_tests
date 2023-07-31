ce9753ee3aa2e078c8ff8762c1a843ed2d089028
Ivan-Dell <vanbog@mail.ru>
2023-07-31 23:51:06 +0300

    codestyle updated

--- dbo.vw_SKUPrice.sql
+++ dbo.vw_SKUPrice.sql
@@ -1,15 +1,16 @@
 use [ics-it_tests]
 
-if OBJECT_ID('dbo.vw_SKUPrice') is not null
+if object_id('dbo.vw_SKUPrice') is not null
 	drop view dbo.vw_SKUPrice;
+
 go
 
 create view dbo.vw_SKUPrice
 as
 select 
-	ID_identity
-	,Code 
-	,Name 
+	s.ID_identity
+	,s.Code 
+	,s.Name 
 	,dbo.udf_GetSKUPrice(ID_identity) as Price
-from dbo.SKU
+from dbo.SKU as s
 
