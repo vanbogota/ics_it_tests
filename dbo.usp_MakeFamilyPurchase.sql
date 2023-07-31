31f21cecb344efc5fd4585fcba6d0100c97d80ca
Ivan-Dell <vanbog@mail.ru>
2023-08-01 00:43:35 +0300

    codestyle updated

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -1,44 +1,40 @@
 use [ics-it_tests]
 
-if OBJECT_ID('dbo.usp_MakeFamilyPurchase','P') is not null
-	drop proc dbo.usp_MakeFamilyPurchase;
+if object_id('dbo.usp_MakeFamilyPurchase','P') is not null
+	drop proc dbo.usp_MakeFamilyPurchase
+
 go
 
 create proc dbo.usp_MakeFamilyPurchase
 	@FamilySurName varchar(255)
 as
+set nocount on
+declare 
+	@FamilyID int
+	,@FamilyBudget money
+	,@TotalValue money
+
+select 
+	@FamilyID = f.ID_identity
+from dbo.Family as f
+where f.SurName = @FamilySurName 
+
+if @FamilyID is null
 begin
-	set nocount on
-
-	declare 
-		@FamilyID int
-
-	select 
-		@FamilyID = ID_identity
-	from dbo.Family
-	where SurName = @FamilySurName 
-
-	if @FamilyID is null
-		begin
-			print N'Ошибка. Такой семьи нет.';
-		end
-
-	declare 
-		@FamilyBudget money
-	declare
-		@TotalValue money
-
-	select 
-		@FamilyBudget = BudgetValue
-	from dbo.Family
-	where ID_identity = @FamilyID;
-
-	select 
-		@TotalValue = sum(Value)
-	from dbo.Basket
-	where ID_Family = @FamilyID;
-
-	update dbo.Family
-	set BudgetValue = @FamilyBudget - @TotalValue
-	where ID_identity = @FamilyID
+	print 'Ошибка. Такой семьи нет.'
 end
+
+select 
+	@FamilyBudget = f.BudgetValue
+from dbo.Family as f
+where f.ID_identity = @FamilyID;
+
+select 
+	@TotalValue = sum(b.Value)
+from dbo.Basket as b
+where b.ID_Family = @FamilyID;
+
+update dbo.Family
+set BudgetValue = @FamilyBudget - @TotalValue
+where ID_identity = @FamilyID
+
