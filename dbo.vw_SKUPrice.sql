a9864af421bad6041796c972456d92a1302d56a4
Ivan-Dell <vanbog@mail.ru>
2023-08-01 00:56:18 +0300

    codestyle updated

--- dbo.vw_SKUPrice.sql
+++ dbo.vw_SKUPrice.sql
@@ -5,12 +5,13 @@ if object_id('dbo.vw_SKUPrice') is not null
 
 go
 
+-- Представление возвращает все атрибуты продуктов из таблицы подуктов и расчетный атрибут со стоимостью одного продукта
 create view dbo.vw_SKUPrice
 as
 select 
 	s.ID_identity
 	,s.Code 
 	,s.Name 
-	,dbo.udf_GetSKUPrice(ID_identity) as Price
+	,dbo.udf_GetSKUPrice(s.ID_identity) as Price
 from dbo.SKU as s
 
