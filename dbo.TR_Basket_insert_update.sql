2092247105c66e2c6fab0bd0d58c30034137d80b
Ivan-Dell <vanbog@mail.ru>
2023-08-01 00:53:25 +0300

    codestyle updated

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -1,21 +1,22 @@
 use [ics-it_tests]
 
-if OBJECT_ID('dbo.TR_Basket_insert_update') is not null
-drop trigger dbo.TR_Basket_insert_update
+if object_id('dbo.TR_Basket_insert_update') is not null
+	drop trigger dbo.TR_Basket_insert_update
+
 go
 
-create trigger dbo.TR_Basket_insert_update
-on dbo.Basket
+-- Расчет значения скидки, если корзину за раз добавляются 2 и более записей одного продукта
+create trigger dbo.TR_Basket_insert_update on dbo.Basket
 after insert, update
 as
 begin
 	set nocount on;
 	update b
-	set DiscountValue = case
+	set b.DiscountValue = case
 							when (select count(ID_SKU) from inserted) >= 2
-							then b.Value * 0.05
+								then b.Value * 0.05
 							else 0
 						end
 	from dbo.Basket as b
-	join inserted as i on b.ID_identity= i.ID_identity
+		inner join inserted as i on i.ID_identity = b.ID_identity
 end
